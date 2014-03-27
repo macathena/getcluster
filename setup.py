0f0ab6ca2a7824458c2c24cb623da23d998f73c3
Victor Vasiliev <vasilvv@mit.edu>
2014-03-27 18:12:00 -0400

    Replace makefile with setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='getcluster',
+      version='10.2.1',
+      author='Debathena Project',
+      author_email='debathena@mit.edu',
+      scripts=['getcluster'],
+      )
