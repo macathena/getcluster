4107361cf8cc8c4c519a5805c28581ffe2072b15
Miriam Rittenberg <mrittenb@mit.edu>
2020-08-23 13:13:17 -0400

    python3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='getcluster',
-      version='10.2.2',
+      version='10.3',
       author='Debathena Project',
       author_email='debathena@mit.edu',
       scripts=['getcluster'],
